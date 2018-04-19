0914d060bbbe89596123e047d81e97d6300e0f6e
Valentin Andruschenko <valentin.andruschenko@digitalarrowtech.com>
2018-04-19 22:46:34 +0300

    Issue #49: Encryption class

---
+++ src/modules/Encryption.js
@@ -0,0 +1,34 @@
+import crypto from 'crypto';
+
+export default class Encryption {
+  alg = 'aes-256-cbc';
+  prefix = '__ENCRYPTED__';
+
+  constructor(encryptionKeyStore) {
+    this.encryptionKeyStore = encryptionKeyStore;
+  }
+
+  encrypt(encryptionKeyId, eventData) {
+    const cipher = this.cipher(this.findKey(encryptionKeyId), eventData);
+    return `${this.prefix}${JSON.stringify({encryptionKeyId, data: cipher})}`;
+  }
+
+  decrypt(encryptedEventData) {
+    const { encryptionKeyId, data } = JSON.parse(encryptedEventData.split(this.prefix)[1]);
+    return this.decipher(this.findKey(encryptionKeyId), data);
+  }
+
+  cipher(key, text) {
+    const cipher = crypto.createCipher(this.alg, key);
+    return cipher.update(text, 'utf-8', 'hex') + cipher.final('hex');
+  }
+
+  decipher(key, text) {
+    const decipher = crypto.createDecipher(this.alg, key);
+    return decipher.update(text, 'hex', 'utf-8') + decipher.final('utf-8');
+  }
+
+  findKey(id) {
+    return this.encryptionKeyStore[id];
+  }
+}
---
+++ test/Encryption-spec.js
@@ -0,0 +1,43 @@
+'use strict';
+const { expect } = require('chai');
+const Encryption = require('../src/modules/Encryption');
+
+const keyId = 'id';
+const keySecret = 'secret';
+const encryptionKeyStore = { [keyId]: keySecret };
+const encryptedPrefix = '__ENCRYPTED__';
+
+const encryption = new Encryption(encryptionKeyStore);
+describe('Encryption', () => {
+  it('should check structure', () => {
+    console.log('encryption:', encryption);
+    expect(encryption).to.have.ownProperty('alg');
+    expect(encryption).to.have.ownProperty('prefix');
+    expect(encryption.prefix).to.equal(encryptedPrefix);
+    expect(encryption).to.have.ownProperty('encryptionKeyStore');
+    expect(encryption.encryptionKeyStore).to.deep.equal(encryptionKeyStore);
+    expect(encryption.encrypt).to.be.a('function');
+    expect(encryption.decrypt).to.be.a('function');
+    expect(encryption.cipher).to.be.a('function');
+    expect(encryption.decipher).to.be.a('function');
+  });
+
+  it('should cipher and decipher', () => {
+    const text = 'secret text';
+    const cipher = encryption.cipher(encryptionKeyStore[keyId], text);
+    const decipher = encryption.decipher(encryptionKeyStore[keyId], cipher);
+    expect(decipher).to.equal(text);
+  });
+
+  it('should encrypt and decrypt', () => {
+    const eventData = { a: '1', b: 2 };
+    const eventDataString = JSON.stringify(eventData);
+    const encryptedEventData = encryption.encrypt(keyId, eventDataString);
+    const cipher = encryption.cipher(encryptionKeyStore[keyId], eventDataString);
+    expect(encryptedEventData).to.equal(`${encryptedPrefix}${JSON.stringify({ encryptionKeyId: keyId, data: cipher })}`);
+
+    const decryptedEventData = encryption.decrypt(encryptedEventData);
+    expect(decryptedEventData).to.equal(eventDataString);
+  });
+});
+
