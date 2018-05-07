865f8ccb156f48269d46c650271cf755418aca92
Valentin Andruschenko <valentin.andruschenko@digitalarrowtech.com>
2018-05-07 19:29:37 +0300

    Export Encryption

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import EventDispatcher from './modules/EventDispatcher';
 import EventTypeSwimlaneDispatcher from './modules/EventTypeSwimlaneDispatcher';
 import EventuateSubscriptionManager from './modules/EventuateSubscriptionManager';
 import EventuateClientConfiguration from './modules/EventuateClientConfiguration';
+import Encryption from './modules/Encryption';
 
 module.exports = EventuateClient;
 module.exports.AggregateRepository = AggregateRepository;
 module.exports.EventuateSubscriptionManager = EventuateSubscriptionManager;
 module.exports.EventuateClientConfiguration = EventuateClientConfiguration;
+module.exports.Encryption = Encryption;
